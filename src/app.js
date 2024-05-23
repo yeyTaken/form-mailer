e88b143fe1f5b0bd9ebdcf5802ba9b3c98ff9614
yeyTaken <yeytaken@outlook.com.br>
2024-05-23 16:39:58 -0300

    update: nova UI pro form

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 const indexRouter = require('./routes/index');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'assets', 'views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/', indexRouter);
 
 app.listen(port, () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,32 +3,103 @@ const router = express.Router();
 const transporter = require('../config/mailer');
 const path = require('path');
 
+router.use(express.json());
+
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../assets/views/form.html'));
 });
 
-router.post('/submit', (req, res) => {
-    const { name, email } = req.body;
+router.get('/confirm', async (req, res) => {
+    const { id, fullName, email, role, area, reason, birthdate } = req.query;
+
+    try {
+        const response = await import('node-fetch');
+        const fetch = response.default;
+        const discordResponse = await fetch(`https://apiattt.discloud.app/user/${id}`);
+        const data = await discordResponse.json();
+
+        if (data.user) {
+            const userInfo = data.user;
+            res.render('confirm', {
+                userInfo,
+                id,
+                fullName,
+                email,
+                role,
+                area,
+                reason,
+                birthdate
+            });
+        } else {
+            res.send(`
+                <p>Usuário não encontrado. <a href="/">Tente novamente.</a></p>
+            `);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Erro ao verificar o usuário.');
+    }
+});
+
+router.post('/submit', async (req, res) => {
+    const { fullName, email, discordId, role, area, reason, birthdate } = req.body;
+
+    // Verificar idade
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    if (age < 15) {
+        return res.status(400).json({ message: 'Você deve ter pelo menos 15 anos para enviar este formulário.' });
+    }
+
+    const response = await import('node-fetch');
+    const fetch = response.default;
+    const discordResponse = await fetch(`https://apiattt.discloud.app/user/${discordId}`);
+    const data = await discordResponse.json();
+    const user = data.user;
 
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: process.env.EMAIL_USER,
-        subject: 'New Form Submission',
+        subject: 'Nova Submissão de Formulário',
         html: `
-            <p>Name: ${name}</p>
-            <p>Email: ${email}</p>
-            <p><a href="http://localhost:3000/respond?response=accept&email=${email}">Aceitar</a></p>
-            <p><a href="http://localhost:3000/respond?response=reject&email=${email}">Recusar</a></p>
+        <div align="center">
+            <img src="${user.avatarUrl}" alt="Foto de Perfil" style="border-radius: 50%; width: 100px;">
+        </div>
+        
+        <div style="text-align: center;">
+            <h1>${user.global_name}</h1>
+            <p><strong>Nome:</strong> ${fullName}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Discord ID:</strong> ${user.username} (${discordId})</p>
+            <p><strong>Papel:</strong> ${role}</p>
+            <p><strong>Área:</strong> ${area}</p>
+            <p><strong>Motivo:</strong> ${reason}</p>
+            <p><strong>Data de Nascimento:</strong> ${birthdate}</p>
+        </div>
+        
+        <div style="text-align: center;">
+            <h1>Ações</h1>
+            <a href="http://localhost:3000/respond?response=accept&email=${email}" style="display: inline-block; padding: 10px 20px; margin: 10px; background-color: #5865F2; color: #FFFFFF; text-decoration: none; border-radius: 5px;">Aceitar</a>
+            <a href="http://localhost:3000/respond?response=reject&email=${email}" style="display: inline-block; padding: 10px 20px; margin: 10px; background-color: #ED4245; color: #FFFFFF; text-decoration: none; border-radius: 5px;">Recusar</a>
+        </div>
         `
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            return res.status(500).send('Error sending email');
+            console.error('Erro ao enviar email:', error);
+            return res.status(500).send('Erro ao enviar o formulário.');
         }
-        res.send('Form submitted successfully');
+        const uri = user.avatarUrl;
+        res.render('submit', { email, uri }); // Renderiza a página de confirmação
     });
 });
+
 
 router.get('/respond', (req, res) => {
     const { response, email } = req.query;
